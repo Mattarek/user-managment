f3499a5905242791a500ec7af30b9ee583da0d27
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-12-31 11:20:08 +0100

    AsyncButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import './App.css';
+import 'normalize.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { DashboardLayout } from './layout/DasbhoardLayout';
 
 function App() {
-    return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<h1>Dashboard Home</h1>} />
+          <Route path="patients" element={<h1>Patients</h1>} />
+          <Route path="patients/add" element={<h1>Add patient</h1>} />
+
+          <Route path="products" element={<h1>Products</h1>} />
+          <Route path="products/add" element={<h1>Add product</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Topbar.tsx
@@ -0,0 +1,86 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Avatar,
+  Menu,
+  MenuItem,
+  IconButton,
+  Popover,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft'; // 👈 tu zmiana
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+export function Topbar() {
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+  const [langAnchor, setLangAnchor] = useState<null | HTMLElement>(null);
+
+  const { i18n, t } = useTranslation();
+
+  return (
+    <AppBar position="fixed" sx={{ ml: 260, zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <DashboardIcon fontSize="large" />
+
+        <div style={{ display: 'flex', alignItems: 'center', gap: 12 }}>
+          <Typography>przyklad@testowy.pl</Typography>
+          <Avatar />
+
+          <IconButton onClick={(e) => setAnchor(e.currentTarget)}>
+            <SettingsIcon sx={{ color: '#fff' }} />
+          </IconButton>
+
+          <Menu anchorEl={anchor} open={!!anchor} onClose={() => setAnchor(null)}>
+            <MenuItem onClick={(e) => setLangAnchor(e.currentTarget)}>
+              <ArrowLeftIcon style={{ marginRight: 8 }} />
+              <ListItemText>{t('topbar.language')}</ListItemText>
+            </MenuItem>
+            <MenuItem onClick={() => console.log('logout')}>{t('topbar.logout')}</MenuItem>
+          </Menu>
+
+          <Popover
+            open={!!langAnchor}
+            anchorEl={langAnchor}
+            onClose={() => setLangAnchor(null)}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            sx={{ mt: -1 }}
+          >
+            <MenuItem
+              onClick={() => {
+                i18n.changeLanguage('pl');
+                setLangAnchor(null);
+                setAnchor(null);
+              }}
+            >
+              <ListItemIcon>🇵🇱</ListItemIcon>
+              <ListItemText>Polski</ListItemText>
+            </MenuItem>
+
+            <MenuItem
+              onClick={() => {
+                i18n.changeLanguage('en');
+                setLangAnchor(null);
+                setAnchor(null);
+              }}
+            >
+              <ListItemIcon>🇬🇧</ListItemIcon>
+              <ListItemText>English</ListItemText>
+            </MenuItem>
+          </Popover>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
